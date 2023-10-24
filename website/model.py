062e6814d77fcf7551c5ca310b44e9011c58e87c
Vartika2401 <97868917+Vartika2401@users.noreply.github.com>
2023-10-25 05:21:34 +0530

    Update model.py

--- website/model.py
+++ website/model.py
@@ -1,29 +1,33 @@
 import openai  # for calling the OpenAI API
 
-# openai.api_key = "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"
+
 
 # models
 
 
 
 def get_response(query, context):
-    return "hey"
-    # EMBEDDING_MODEL = "text-embedding-ada-002"
-    # GPT_MODEL = "gpt-3.5-turbo"
-    # context = context
-    # query = f"""Use the below context to answer the subsequent question. If the answer cannot be found, write "I don't know."
-    # Article:
-    # \"\"\"
-    # {context}
-    # \"\"\"
-    # Question = {query}"""
-    # response = openai.ChatCompletion.create(
-    #     messages=[
-    #       {'role': 'system', 'content': 'your answer about the question.'},
-    #         {'role': 'user', 'content': query},
-    #     ],
-    #     model=GPT_MODEL,
-    #     temperature=0,
-    # )
+    # if query == "summarize all the reviews":
+    #     return "The product is good"
+    # else:
+    #     return "okay"
+    openai.api_key = "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"
+    EMBEDDING_MODEL = "text-embedding-ada-002"
+    GPT_MODEL = "gpt-3.5-turbo"
+    context = context
+    query = f"""Use the below context to answer the subsequent question. If the answer cannot be found, write "I don't know."
+    Article:
+    \"\"\"
+    {context}
+    \"\"\"
+    Question = {query}"""
+    response = openai.ChatCompletion.create(
+        messages=[
+          {'role': 'system', 'content': 'your answer about the question.'},
+            {'role': 'user', 'content': query},
+        ],
+        model=GPT_MODEL,
+        temperature=0,
+    )
 
-    # return response['choices'][0]['message']['content']
+    return response['choices'][0]['message']['content']
